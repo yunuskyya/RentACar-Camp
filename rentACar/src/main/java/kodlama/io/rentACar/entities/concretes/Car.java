d5dd84e5860c97a13b191eca8b7048b1767fbe45
yunuskyya <0yunuskaya0@gmail.com>
2024-03-14 19:30:07 +0300

    Entity, car sınıfı oluşturuldu.

--- rentACar/src/main/java/kodlama/io/rentACar/entities/concretes/Car.java
+++ rentACar/src/main/java/kodlama/io/rentACar/entities/concretes/Car.java
@@ -6,25 +6,31 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import java.util.List;
-
-@Table(name="models")
+@Table(name="cars")
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
-public class Model {
+public class Car {
     @Id
     @Column(name="id")
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
-    @Column(name="name")
-    private String name;
-    @ManyToOne
-    @JoinColumn(name = "brand_id")
-    private Brand brand;
 
-    @OneToMany(mappedBy = "model")
-    private List <Car> cars;
+    @Column(name = "plate")
+    private String plate;
+
+    @Column(name = "dailyPrice")
+    private String dailyPrice;
+
+    @Column(name = "modelYear")
+    private int modelYear;
+
+    @Column(name = "state")
+    private int state;
+
+    @ManyToOne
+    @JoinColumn(name = "model_id")
+    private Model model;
 }
