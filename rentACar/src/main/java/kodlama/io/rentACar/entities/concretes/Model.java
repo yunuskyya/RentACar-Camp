2e3272f473c97ed9c65bd4b598e847ad278a4f5b
yunuskyya <0yunuskaya0@gmail.com>
2024-03-14 18:59:07 +0300

    model varlığı olutruldu. OneToMany ve ManyToOne ilişkileri kuruldu

--- rentACar/src/main/java/kodlama/io/rentACar/entities/concretes/Model.java
+++ rentACar/src/main/java/kodlama/io/rentACar/entities/concretes/Model.java
@@ -1,25 +1,25 @@
 package kodlama.io.rentACar.entities.concretes;
 
 import jakarta.persistence.*;
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
-import java.util.List;
-
-@Table(name="brands")
+@Table(name="models")
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
-public class Brand {
+public class Model {
     @Id
     @Column(name="id")
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     @Column(name="name")
     private String name;
-
-    @OneToMany(mappedBy = "brand")
-    List<Model> models;
-
+    @ManyToOne
+    @JoinColumn(name = "brand_id")
+    private Brand brand;
 }
